702d0d6b089d3644cea3349f1774dd2fc696326a
SharonyuFu <u8528816@gmail.com>
2016-06-06 17:31:56 +0800

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -61,7 +61,7 @@ $( "#dropdown li a" ).each(function( index,element) {
   $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+city+'")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',function(data){
     var temp=data.query.results.channel.item.condition.temp;
     $(element).text($(element).text()+cTof(temp)+" ℃").addClass("temp");
-    $(element).text($(element).text()+"&nbsp"+cTof(temp));
+    $(element).text($(element).text()+"  "+cTof(temp));
     // $(element).text($(element).text()+"  "+cTof(temp));
     // $(element).append("<span class="2"></span>");
  
